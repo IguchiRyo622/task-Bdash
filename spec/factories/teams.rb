f8bdebd6c2aa001b44d247f7d4f6cda94dd083cd
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-19 02:31:21 +0900

    team作成の単体テストコード

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :team do
-    
+    team_name              {Faker::Name.initials(number: 2)}
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
-
-RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Team do
+  before do
+    @team = FactoryBot.build(:team)
+  end
+  describe 'チーム作成' do
+    context 'チーム作成がうまくいくとき' do
+      it 'team_nameが存在すれば作成できる' do
+        expect(@team).to be_valid
+      end
+    end
+    context 'チーム作成がうまくいかないとき' do
+      it 'team_nameが空だと作成できない' do
+        @team.team_name = nil
+        @team.valid?
+        expect(@team.errors.full_messages).to include('チーム名を入力してください')
+      end
+    end
+  end
 end
