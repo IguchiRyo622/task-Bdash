232d021f4c82c005ad8ce9405e287ff939d9a517
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-19 02:00:57 +0900

    チーム名をトップページに表示

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/index'
   devise_for :users
-  root to: "lists#index"
+  root to: "teams#index"
   resources :teams, only: [:new, :create]
 end
