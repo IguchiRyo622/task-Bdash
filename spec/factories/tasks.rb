7a57c17963acb767e9996229982d4167985f314b
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-23 15:04:25 +0900

    task作成の単体テストコード

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    task_name              {Faker::Team.name}
+    content                {"ここにタスク内容を書きます"}
+    target_date            {Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}
+    task_item              {"ここにタスクの要点を書きます"}
+    association :list
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
-
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Task do
+  before do
+    @task = FactoryBot.build(:task)
+  end
+  describe 'タスク作成' do
+    context 'タスク作成がうまくいくとき' do
+      it 'task_name,content,target_date,task_itemが存在すれば作成できる' do
+        expect(@task).to be_valid
+      end
+      it 'task_name以外空でも作成できる' do
+        @task.content = nil
+        @task.target_date = nil
+        @task.task_item = nil
+        expect(@task).to be_valid
+      end
+    end
+    context 'タスク作成がうまくいかないとき' do
+      it 'task_nameが空だと作成できない' do
+        @task.task_name = nil
+        @task.valid?
+        expect(@task.errors.full_messages).to include('タスク名を入力してください')
+      end
+    end
+  end
 end
