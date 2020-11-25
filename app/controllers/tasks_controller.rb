25b95256c8481070e9205fe2f9bd68a81524b864
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-25 12:26:04 +0900

    コントローラーを編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new]
   before_action :setting_team_list, only: [:index, :new, :create, :show, :edit, :edit, :update]
+  before_action :setting_task, only: [:show, :edit, :update]
+  before_action :setting_includes, only: [:index, :show, :edit]
 
   def index
-    @lists = @team.lists.includes(:user)
-    @tasks = @list.tasks.includes(:list)
     @task = Task.find_by(params[:task_id])
   end
 
@@ -22,20 +22,13 @@ class TasksController < ApplicationController
   end
 
   def show
-    @lists = @team.lists.includes(:user)
-    @tasks = @list.tasks.includes(:list)
-    @task = Task.find(params[:id])
     @user = User.find_by(id: @task.user_id)
   end
 
   def edit
-    @lists = @team.lists.includes(:user)
-    @tasks = @list.tasks.includes(:list)
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_update)
       redirect_to team_list_task_path(@team, @list, @task)
     else
@@ -44,6 +37,7 @@ class TasksController < ApplicationController
   end
 
   private
+  
   def task_params
     params.require(:task).permit(:task_name, :content, :target_date, :task_item).merge(user_id: current_user.id)
   end
@@ -55,5 +49,14 @@ class TasksController < ApplicationController
   def setting_team_list
     @team = Team.find(params[:team_id])
     @list = List.find(params[:list_id])
+  end
+
+  def setting_task
+    @task = Task.find(params[:id])
+  end
+
+  def setting_includes
+    @lists = @team.lists.includes(:user)
+    @tasks = @list.tasks.includes(:list)
   end
 end
