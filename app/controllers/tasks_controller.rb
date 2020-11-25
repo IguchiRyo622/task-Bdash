89c1105f4f90fae4b8dac866f8dfb9edcafd1651
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-25 12:21:57 +0900

    updateアクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new]
-  before_action :setting_team_list, only: [:index, :new, :create, :show, :edit]
+  before_action :setting_team_list, only: [:index, :new, :create, :show, :edit, :edit, :update]
 
   def index
     @lists = @team.lists.includes(:user)
@@ -34,10 +34,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_update)
+      redirect_to team_list_task_path(@team, @list, @task)
+    else
+      render :edit
+    end
+  end
 
   private
   def task_params
     params.require(:task).permit(:task_name, :content, :target_date, :task_item).merge(user_id: current_user.id)
+  end
+
+  def task_update
+    params.require(:task).permit(:task_name, :content, :target_date, :report, :final_report, :task_item).merge(user_id: current_user.id)
   end
 
   def setting_team_list
